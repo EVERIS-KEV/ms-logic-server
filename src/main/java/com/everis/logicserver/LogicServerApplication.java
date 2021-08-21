677c5596662ca6a2989732377e7051e6b314a28f
Mauricio06Everis <kpalmall@everis.com>
2021-08-21 17:42:25 -0500

    Update 3

--- src/main/java/com/everis/logicserver/LogicServerApplication.java
+++ src/main/java/com/everis/logicserver/LogicServerApplication.java
@@ -2,13 +2,20 @@ package com.everis.logicserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient; 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class LogicServerApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(LogicServerApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(LogicServerApplication.class, args);
+
+		int x = 1;
+		if ((x % 2) == 0) {
+			System.out.println("Es par.");
+		} else {
+			System.out.println("No es par.");
+		}
+	}
 }
